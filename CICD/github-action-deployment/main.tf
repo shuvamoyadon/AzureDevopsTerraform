33210fe24f436c7449f5fdb40aceca1a50c30ba3
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2023-09-06 23:08:10 +0530

    added azure deployment

--- CICD/github-action-deployment/main.tf
+++ CICD/github-action-deployment/main.tf
@@ -1,13 +1,13 @@
-# module "azure_databricks_demo" {
-#   source       = "../../modules/databricks-clusters"
-#   cluster_name = var.cluster_name
-#   department   = var.department
-#   user_names   = var.user_names
-#   group_name   = var.group_name
-#   resource_group_name = var.resource_group_name
-#   location = var.location
-#   tags         = var.tags
-# }
+module "azure_databricks_demo" {
+  source       = "../../modules/databricks-clusters"
+  cluster_name = var.cluster_name
+  department   = var.department
+  user_names   = var.user_names
+  group_name   = var.group_name
+  resource_group_name = var.resource_group_name
+  location = var.location
+  tags         = var.tags
+}
 
 module "azure_storage" {
   source                 = "../../modules/storage"
